07507fcd2ed3529c0f23ea653d9dd30f5162512b
xhiny <axhiny@163.com>
2023-02-20 10:47:46 +0800

    启动时先清除Xrog

--- startServer.sh
+++ startServer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+find /tmp -name .X0-lock | xargs  rm -rf
+
 nohup X :0 -config /var/www/convert-document/dummy.conf > /dev/null 2>&1 &
 
 nohup /opt/kingsoft/wps-office/office6/wps -multiply > /dev/null 2>&1 &
